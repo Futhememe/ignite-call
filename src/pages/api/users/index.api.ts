18e60cea442050e09f7e93c3b895fe3ce72876a2
Futhememe <kyoro_souza@hotmail.com>
2023-01-02 16:39:46 -0300

    🎨 verify if a user exists

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -12,6 +12,18 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'user already exists',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
